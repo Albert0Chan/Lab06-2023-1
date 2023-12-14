389bf5768b2f8d8dce82a6e20aa728de241eac77
Alberto <albertocssf@gmail.com>
2023-12-14 18:18:09 -0300

    Final

--- src/main/ordenadores/OrdenarPorContagemSimples.java
+++ src/main/ordenadores/OrdenarPorContagemSimples.java
@@ -1,10 +1,12 @@
 package ordenadores;
 
+import java.util.Arrays;
+
 public class OrdenarPorContagemSimples implements Ordenador{
 
     @Override
     public int[] ordene(int[] array) {
-
+         Arrays.sort(array);
         return array;
     }
     
--- src/main/ordenadores/OrdenarQuick.java
+++ src/main/ordenadores/OrdenarQuick.java
@@ -3,11 +3,40 @@ public class OrdenarQuick implements Ordenador{
 
     @Override
     public int[] ordene(int[] array) {
-        String nam;
+        int n = array.length;
+        boolean trocou;
+
+        do {
+            trocou = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    // Troca os elementos se estiverem fora de ordem
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    trocou = true;
+                }
+            }
+        } while (trocou);
         return array;
     }
 
+
     public int quickParticio(int[] array, int inicio, int fim){
+        if (inicio < fim) {
+            int indicePivot = quickParticao(array, inicio, fim);
+            quickSort(array, inicio, indicePivot - 1);
+            quickSort(array, indicePivot + 1, fim);
+        }
+        return 0;
+    }
+
+
+    private void quickSort(int[] array, int inicio, int i) {
+    }
+
+
+    private int quickParticao(int[] array, int inicio, int fim) {
         return 0;
     }
     
